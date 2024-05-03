fa58b5b5d33df9d7b786421cb5b2ba352b9d719f
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2024-05-03 11:59:49 +0300

    Update Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -8,8 +8,8 @@ export default function Form() {
     const [name, setName] = useState('')
     const [number, setNumber] = useState('')
 
-    const handleInputChange = evt => {
-        const { name, value } = evt.target
+    const handleInputChange = e => {
+        const { name, value } = e.target
 
         switch (name) {
             case 'name':
@@ -25,17 +25,28 @@ export default function Form() {
         }
     }
 
+    const locaStorage = (contacts) => {
+        localStorage.setItem(name, number)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setName('')
+        setNumber('')
+        locaStorage()
+    }
+
     return (
         <div className={css.blok__form}>
-            <form className={css.form} onSubmit={() => null}>
+            <form className={css.form} onSubmit={handleSubmit}>
                 <label className={css.label}>
                     Name
                     <input
                         className={css.input}
                         onChange={handleInputChange}
+                        name="name"
                         value={name}
                         type="text"
-                        name="name"
                         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                         required
@@ -46,9 +57,9 @@ export default function Form() {
                     <input
                         className={css.input}
                         onChange={handleInputChange}
+                        name="number"
                         value={number}
                         type="tel"
-                        name="number"
                         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                         required
